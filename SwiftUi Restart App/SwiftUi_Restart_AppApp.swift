fdb1375fdec02fe47da5a02157d71d3a5b41100d
fahmi <fahmiabdulaziz44@gmail.com>
2023-08-15 20:07:44 +0700

    Initial Commit

---
+++ SwiftUi Restart App/SwiftUi_Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_Restart_AppApp.swift
+//  SwiftUi Restart App
+//
+//  Created by Fahmi Aziz on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
